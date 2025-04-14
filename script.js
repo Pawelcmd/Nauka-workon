35a4e86bc39ab895b692a357fc7d710a45e1f4ad
Pawel <pawelkaminski155@gmail.com>
2025-04-14 21:13:07 +0200

    blabla

--- script.js
+++ script.js
@@ -1,111 +1,85 @@
-const changeMessage = document.querySelector('#message');
-const btn = document.querySelector('#changeBtn');
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response) => {
+        document.querySelector('#loading').style.display = "none";
 
-changeMessage.textContent = "Hello Word"
-
-btn.addEventListener('click', () => {
-    changeMessage.style.color = 'red'
-})
-
-//////////////////////////////////////////////////////////
-
-
-
-const addBtn = document.querySelector('#addBtn');
-const input = document.querySelector('#itemInput');
-const ul = document.querySelector('#itemList');
-
-function addItem() {
-    const value = input.value.trim();
-
-    if (value !== "") {
-        const newElement = document.createElement('li');
-        newElement.textContent = value;
-        ul.appendChild(newElement);
-        input.value = '';
-    }
-}
-
-addBtn.addEventListener('click', addItem);
-
-input.addEventListener('keydown', (event) => {
-    if (event.key === 'Enter') {
-        addItem();
-    }
-
-});
-
-
-const count = document.querySelector('#count');
-const btnCount = document.querySelector('#btnCount');
-const numbers = document.querySelector('#numbers')
-
-let clickCount = 0;
-
-btnCount.addEventListener('click', () => {
-    clickCount++;
-    numbers.textContent = clickCount
-})
-
-
-
-document.addEventListener('DOMContentLoaded', () => {
-    const body = document.body;
-    const button = document.querySelector('#themeToogle');
-
-    const setTheme = (theme) => {
-        body.classList.remove('light', 'dark');
-        body.classList.add(theme);
-        localStorage.setItem('theme', theme);
-        button.textContent = theme === 'light' ? 'Tryb ciemny' : 'Tryb jasny';
-    };
-
-    const savedTheme = localStorage.getItem('theme');
-    setTheme(savedTheme);
-
-    button.addEventListener('click', () => {
-        const newTheme = body.classList.contains('light') ? 'dark' : 'light';
-        setTheme(newTheme);
+        if (!response.ok) {
+            if (response.status === 404) {
+                document.querySelector('#error-message').innerHTML = "Zawartosc nie istnieje"
+            } else if (response.status === 500) {
+                document.querySelector('#error-message').innerHTML = "Zawartosc nie istnieje"
+            }
+        }
+        return response.json();
+    })
+    .then((data) => {
+        const container = document.querySelector('#user-container');
+        container.innerHTML = data.map(user => `
+            <p>Imię: ${user.name}</p>
+        `).join('');
+    })
+    .catch((error) => {
+        document.querySelector('#error-message').innerHTML = "Błąd: " + error.message;
     });
 
 
-    const counter = document.querySelector('#counter');
-
-    let entryCounter = parseInt(localStorage.getItem('counter')) || 0;
-
-    entryCounter++;
-    localStorage.setItem('counter', entryCounter);
-    counter.textContent = entryCounter;
-});
 
 
-
-const list = [-3, 5, 6];
-
-
-const some = list.some((el) => {
-    return el < 0
-})
-
-console.log(some);
+// fetch("https://jsonplaceholder.typicode.com/posts")
+//     .then(response => response.json(0))
+//     .then((data) => {
+//         data.map(post =>
+//             console.log(post.title)
+//         )
+//     })
 
 
-const namesList = ["Ania", "Hania", "Frania"];
 
-const reduceString = namesList.reduce((acc, el, index) => {
-    console.log(index);
-    if (index < namesList.length - 1) {
-        return acc + el + ", "
+// axios.get("https://jsonplaceholder.typicode.com/posts")
+// .then(response => {
+//     response.data.map(post => {
+//       console.log(post.title);
+//     });
+//   })
+// .catch(error => {
+//     console.error('Błąd:', error.message);
+// });
+
+// Promise.all([
+//     fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`)
+// ])
+// .then(response => response.json(0))
+// .then ((data) => {
+//     data.map(post => {
+//         console.log(post.title);
+//     })
+// })
+
+// const userId = 1;
+// const fetchUseresPosts = (userId) => {
+//     fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`)
+//     .then((response) => response.json())
+//     .then((posts) => console.log(posts.forEach((post) => console.log(post))));
+// };
+
+// fetchUseresPosts(userId);
+
+const postForm = document.querySelector('#postForm')
+const messageDiv = document.querySelector('#message')
+
+postForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const title = postForm.elements.title.value;
+    const body = postForm.elements.body.value;
+    console.log(title, body);
+
+    const response = await axios.post('https://jsonplaceholder.typicode.com/posts',
+        {title, body, userId:1}
+    )
+    console.log(response);
+    if (response.data) {
+        messageDiv.textContent="dane zostaly zapisane"
     }else {
-        return acc + el;
+        messageDiv.textContent = "dane nie zostaly zapisane"
     }
-}, "")
-console.log(reduceString);
-
-const testString = "Dvcvcsvcdv"
-console.log(testString.toUpperCase());
-
-const capitalizedNames = namesList.map((el) => el.toUpperCase())
-console.log(capitalizedNames);
-
-
+    
+});
